1f1c3def6b3338b874b15c32718c94e511ce1bcc
Anaokulu Uygulaması <baltaci.tml@gmail.com>
2024-02-14 11:18:21 +0300

    first commit

---
+++ src/components/TopSection.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react'
+
+export default function TopSection({ submitted,playerName }) {
+  const [classCondition, setClassCondition] = useState(false)
+
+  console.log(playerName)
+
+  useEffect(() => {
+    const elements = document.getElementsByClassName('name-input')
+    setClassCondition(elements.length === 0)
+  }, [])
+  return (
+    <div className='top-section'>
+      <h1 className={submitted && !classCondition ? 'second-fade-in' : null}>
+        {!submitted || classCondition
+          ? 'Efsane'
+          : `Bir kahraman doğdu. Kahramanın adı: ${playerName} `}
+      </h1>
+      <div className='image-container'>
+        <img
+          className={!submitted || classCondition ? null : 'fade-out-and-in'}
+          src={
+            !submitted || classCondition
+              ? './images/character1.png'
+              : './images/character2.png'
+          }
+          alt='Character'
+        />
+      </div>
+    </div>
+  )
+}
